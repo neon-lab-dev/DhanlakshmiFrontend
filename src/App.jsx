2796e8cacb5017483abc1c0541d869f9dd324f2f
Premanand Chowdhury <freudigprem@gmail.com>
2024-04-10 23:48:37 +0530

    chore: Layout Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
-import './styles.css'
+import './styles.css';
+import { Layout } from './components';
 
 const App = () => {
   return (
-    <div className="h-screen flex flex-col items-center justify-center">
-      <h1 className="text-primary">
-        Welcome to Dhanlakshmi
-      </h1>
-      <p className='text-heading w-[55%] text-center mt-4'>Welcome to Dhanlakshmi Organics, your trusted partner in agriculture. One stop solution for Fertilisers, Pesticides and Cattlefeed supplements. Join us in harvesting prosperity.</p>
-    </div>
+    <Layout>
+      <div className="flex justify-center flex-col items-center mt-[20%]">
+        <h1 className="text-primary">Welcome to Dhanlakshmi</h1>
+        <p className="text-heading w-[55%] text-center mt-4">
+          Welcome to Dhanlakshmi Organics, your trusted partner in agriculture.
+          One stop solution for Fertilisers, Pesticides and Cattlefeed
+          supplements. Join us in harvesting prosperity.
+        </p>
+      </div>
+    </Layout>
   );
 };
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
+  return <div className="text-center">Footer</div>;
 }
 
 export default Footer
