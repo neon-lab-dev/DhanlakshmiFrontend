a9874f7bc2985e2752fab355410935074fb8987c
Premanand Chowdhury <freudigprem@gmail.com>
2024-04-12 10:29:36 +0530

    chore: Navabar initial code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import React from 'react';
+
+import logo from '../assets/icons/dhanlakshami.svg';
+import menu from '../assets/icons/menu.svg';
 
 const Header = () => {
-  return <div className="text-center">Header</div>;
-}
+  return (
+    <nav className="flex justify-between border py-[0.625rem] shadow md:my-[40px]">
+      <div className="flex items-center">
+        <img src={logo} alt="Dhanlakshmi" />
+        <div className="W-[12.0625rem] ml-[0.85rem]">
+          <p className="text-accent font-800 text-[20px] leading-[16.15px] mb-1">
+            Dhanlakshmi Organics
+          </p>
+          <p className="text-primary text-[0.6875rem] leading-[16.15px] font-500">
+            Let’s Build Organic
+          </p>
+        </div>
+      </div>
 
-export default Header
+      <div className="hidden md:block">
+        <ul className="flex">
+          <li>Home</li>
+          <li>Products</li>
+          <li>Become a Seller</li>
+          <li>About us</li>
+        </ul>
+      </div>
+
+      <div className="h-[16px] w-[16px] self-center md:hidden">
+        <img src={menu} alt="Hamburger Menu" />
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
---
+++ src/routes/routes.jsx
@@ -0,0 +1,31 @@
+import React, { Suspense } from 'react';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ROUTES } from '.';
+
+// COMPONENT
+import Layout from '../components/Layout';
+
+const RoutesContainer = () => {
+  return (
+    <Router>
+      <Routes>
+        {ROUTES.map((route, index) => (
+          <Route
+            key={index}
+            path={route.path}
+            element={
+              <Layout>
+                <Suspense fallback={<p>Loaing...</p>}>
+                  <route.component />
+                </Suspense>
+              </Layout>
+            }
+          />
+        ))}
+      </Routes>
+    </Router>
+  );
+};
+
+export default RoutesContainer;
