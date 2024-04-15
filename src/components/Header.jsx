ed90246947a02b3c098d3930273829470530b226
Tilak Thapa <iamjrtilak@gmail.com>
2024-04-15 13:14:47 +0545

    feat: complete navbar responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,68 @@
-import React from 'react';
+import React from "react";
 
-import logo from '../assets/icons/dhanlakshami.svg';
-import menu from '../assets/icons/menu.svg';
+import logo from "../assets/icons/dhanlakshami.svg";
+import menu from "../assets/icons/menu.svg";
+import { Link, useLocation } from "react-router-dom";
+
+const HEADER_LINKS = [
+  {
+    label: "Home",
+    link: "/",
+  },
+  {
+    label: "Products",
+    link: "/products",
+  },
+  {
+    label: "Become A Seller",
+    link: "/seller",
+  },
+  {
+    label: "About Us",
+    link: "/about",
+  },
+];
 
 const Header = () => {
+  const { pathname } = useLocation();
   return (
-    <nav className="flex justify-between border py-[0.625rem] shadow md:my-[40px]">
-      <div className="flex items-center">
-        <img src={logo} alt="Dhanlakshmi" />
-        <div className="W-[12.0625rem] ml-[0.85rem]">
-          <p className="text-accent font-800 text-[20px] leading-[16.15px] mb-1">
+    <nav
+      style={{
+        background: "linear-gradient(90deg, #FFF 0%, #FFF 49%, #FFF 100%)",
+        boxShadow: "0px 0px 26px 0px rgba(0, 0, 0, 0.10)",
+      }}
+      className="flex justify-between rounded-[10px] py-4 px-4 sm:px-6 items-center"
+    >
+      <div className="flex items-center justify-center gap-4">
+        <img
+          src={logo}
+          alt="Dhanlakshmi"
+          className="w-[51px] md:w-[78px] h-[41px] md:h-[63px] object-contain object-center"
+        />
+        <div className="flex flex-col gap-1">
+          <p className="text-accent font-800 text-lg md:text-xl lg:text-[30px] lg:leading-[24.527px]">
             Dhanlakshmi Organics
           </p>
-          <p className="text-primary text-[0.6875rem] leading-[16.15px] font-500">
+          <p className="text-primary text-xs sm:text-sm md:text-[16px] leading-[24.527px] font-500 font-Inter">
             Let’s Build Organic
           </p>
         </div>
       </div>
 
-      <div className="hidden md:block">
-        <ul className="flex">
-          <li>Home</li>
-          <li>Products</li>
-          <li>Become a Seller</li>
-          <li>About us</li>
+      <div className="hidden md:block font-Inter">
+        <ul className="flex gap-5 items-center justify-center">
+          {HEADER_LINKS.map(({ label, link }) => (
+            <li
+              key={label}
+              className={`text-base xl:text-lg transition-colors ${
+                pathname === link
+                  ? "text-primary underline font-600 underline-offset-8"
+                  : "text-heading hover:text-primary"
+              }`}
+            >
+              <Link to={link}>{label}</Link>
+            </li>
+          ))}
         </ul>
       </div>
 
