fee95b03691b873fd959b52bed1b8939db5436a8
Tilak Thapa <iamjrtilak@gmail.com>
2024-04-16 11:50:49 +0545

    Refactor Header component to add menu functionality and improve responsiveness

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import logo from "../assets/icons/dhanlakshami.svg";
 import menu from "../assets/icons/menu.svg";
+import close from "../assets/icons/close-svgrepo-com.svg";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const { pathname } = useLocation();
   const navigate = useNavigate();
   const HEADER_LINKS = [
@@ -29,32 +31,81 @@ const Header = () => {
       },
     },
   ];
-  return (
-    <nav
-      style={{
-        background: "linear-gradient(90deg, #FFF 0%, #FFF 49%, #FFF 100%)",
-        boxShadow: "0px 0px 26px 0px rgba(0, 0, 0, 0.10)",
-      }}
-      className="flex justify-between md:rounded-[10px] py-4 px-4 sm:px-6 items-center md:wrapper"
-    >
-      <Link to="/" className="flex items-center justify-center gap-4">
-        <img
-          src={logo}
-          alt="Dhanlakshmi"
-          className="w-[51px] md:w-[78px] h-[41px] md:h-[63px] object-contain object-center"
-        />
-        <div className="flex flex-col md:gap-1">
-          <p className="text-accent font-800 text-lg md:text-xl lg:text-[30px] lg:leading-[24.527px]">
-            Dhanlakshmi Organics
-          </p>
-          <p className="text-primary text-xs sm:text-sm md:text-[16px] md:leading-[24.527px] font-500 font-Inter">
-            Let&apos;s Build Organic
-          </p>
-        </div>
-      </Link>
 
-      <div className="hidden md:block font-Inter">
-        <ul className="flex gap-5 items-center justify-center">
+  useEffect(() => {}, []);
+
+  return (
+    <>
+      <nav
+        style={{
+          background: "linear-gradient(90deg, #FFF 0%, #FFF 49%, #FFF 100%)",
+          boxShadow: "0px 0px 26px 0px rgba(0, 0, 0, 0.10)",
+        }}
+        className="flex justify-between md:rounded-[10px] py-4 z-50 px-4 sm:px-6 items-center md:wrapper"
+      >
+        <Link to="/" className="flex items-center justify-center gap-4">
+          <img
+            src={logo}
+            alt="Dhanlakshmi"
+            className="w-[51px] md:w-[78px] h-[41px] md:h-[63px] object-contain object-center"
+          />
+          <div className="flex flex-col md:gap-1">
+            <p className="text-accent font-800 text-lg md:text-xl lg:text-[30px] lg:leading-[24.527px]">
+              Dhanlakshmi Organics
+            </p>
+            <p className="text-primary text-xs sm:text-sm md:text-[16px] md:leading-[24.527px] font-500 font-Inter">
+              Let&apos;s Build Organic
+            </p>
+          </div>
+        </Link>
+
+        <div className="hidden md:block font-Inter">
+          <ul className="flex gap-5 items-center justify-center">
+            {HEADER_LINKS.map(({ label, link, action }) => (
+              <li
+                key={label}
+                className={`text-base xl:text-lg transition-colors ${
+                  pathname === link
+                    ? "text-primary underline font-600 underline-offset-8"
+                    : "text-heading hover:text-primary"
+                }`}
+              >
+                {link ? (
+                  <Link to={link}>{label}</Link>
+                ) : (
+                  <button onClick={action}>{label}</button>
+                )}
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <button
+          onClick={() => {
+            setIsMenuOpen(!isMenuOpen);
+            document.body.style.overflow = isMenuOpen ? "auto" : "hidden";
+            document
+              .getElementById("mobile-menu")
+              ?.classList.toggle("translate-x-full");
+          }}
+          className="h-[16px] w-[16px] self-center md:hidden text-accent"
+        >
+          <img src={isMenuOpen ? close : menu} alt="Hamburger Menu" />
+        </button>
+      </nav>
+
+      {isMenuOpen && (
+        <div className="md:hidden w-screen h-screen bg-surface opacity-80 backdrop-blur-xl fixed top-0 left-0 z-30" />
+      )}
+      <aside
+        id="mobile-menu"
+        style={{
+          background: "linear-gradient(90deg, #FFF 0%, #FFF 49%, #FFF 100%)",
+          boxShadow: "0px 0px 26px 0px rgba(0, 0, 0, 0.10)",
+        }}
+        className="md:hidden h-screen w-[200px]  z-40 absolute top-0 right-0 pt-[120px] px-6 pb-4 translate-x-full transition-transform"
+      >
+        <ul className="flex gap-10 flex-col justify-center">
           {HEADER_LINKS.map(({ label, link, action }) => (
             <li
               key={label}
@@ -72,12 +123,8 @@ const Header = () => {
             </li>
           ))}
         </ul>
-      </div>
-
-      <div className="h-[16px] w-[16px] self-center md:hidden">
-        <img src={menu} alt="Hamburger Menu" />
-      </div>
-    </nav>
+      </aside>
+    </>
   );
 };
 
