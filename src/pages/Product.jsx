1f669ae7c20cb3954b6da8de96d807d4afeb914c
Tilak Thapa <iamjrtilak@gmail.com>
2024-04-17 10:24:45 +0545

    Refactor Product component to handle error and display no products found message

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -74,78 +74,84 @@ const Product = () => {
             </>
           ) : (
             <>
-              {filteredProducts.length > 0 ? (
-                filteredProducts.map((product, i) => (
-                  <div
-                    key={i}
-                    className="rounded-xl md:flex-row bg-white p-3 md:px-6 md:py-4 flex flex-col gap-5 2xl:gap-10 md:min-h-[200px]"
-                    style={{
-                      boxShadow: "0px 0px 3.091px 0px rgba(0, 0, 0, 0.25)",
-                    }}
-                  >
-                    <div className="h-[253px] md:m-auto md:max-h-[188px] md:min-w-[241px] md:w-[241px] w-full relative">
-                      <img
-                        src={product.avatar.url}
-                        alt={product.title}
-                        className="w-full h-full object-contain object-center absolute"
-                      />
-                    </div>
-                    <div className="flex flex-col gap-2">
-                      <h3 className="text-lg font-Inter font-700">
-                        {product.name}
-                      </h3>
-                      <p className="xl:hidden">
-                        {product.description.length > 100 ? (
-                          <>
-                            <span>
-                              {isReadMore
-                                ? product.description
-                                : product.description.slice(0, 100) + "..."}
-                            </span>
-                            <button
-                              onClick={() => setIsReadMore(!isReadMore)}
-                              className="text-base font-600 ml-2"
-                            >
-                              {isReadMore ? "Read Less" : "Read More"}
-                            </button>
-                          </>
-                        ) : (
-                          product.description
-                        )}
-                      </p>
-                      <p className="hidden xl:block">{product.description}</p>
-                      <div className="flex gap-6 flex-col md:flex-row-reverse md:mr-auto md:items-center">
-                        <div className="flex flex-col gap-1">
-                          <h4 className="text-base text-primary font-500">
-                            Available in packs of
-                          </h4>
-                          <div className="flex gap-2 flex-wrap">
-                            {product.available_packs
-                              ?.split(",")
-                              .map((pack, i) => (
-                                <div
-                                  key={i}
-                                  className="text-base text-bodyText rounded bg-muted font-600 px-4 py-2"
-                                >
-                                  {pack}
-                                </div>
-                              ))}
+              {!isError ? (
+                filteredProducts.length > 0 ? (
+                  filteredProducts.map((product, i) => (
+                    <div
+                      key={i}
+                      className="rounded-xl md:flex-row bg-white p-3 md:px-6 md:py-4 flex flex-col gap-5 2xl:gap-10 md:min-h-[200px]"
+                      style={{
+                        boxShadow: "0px 0px 3.091px 0px rgba(0, 0, 0, 0.25)",
+                      }}
+                    >
+                      <div className="h-[253px] md:m-auto md:max-h-[188px] md:min-w-[241px] md:w-[241px] w-full relative">
+                        <img
+                          src={product.avatar.url}
+                          alt={product.title}
+                          className="w-full h-full object-contain object-center absolute"
+                        />
+                      </div>
+                      <div className="flex flex-col gap-2">
+                        <h3 className="text-lg font-Inter font-700">
+                          {product.name}
+                        </h3>
+                        <p className="xl:hidden">
+                          {product.description.length > 100 ? (
+                            <>
+                              <span>
+                                {isReadMore
+                                  ? product.description
+                                  : product.description.slice(0, 100) + "..."}
+                              </span>
+                              <button
+                                onClick={() => setIsReadMore(!isReadMore)}
+                                className="text-base font-600 ml-2"
+                              >
+                                {isReadMore ? "Read Less" : "Read More"}
+                              </button>
+                            </>
+                          ) : (
+                            product.description
+                          )}
+                        </p>
+                        <p className="hidden xl:block">{product.description}</p>
+                        <div className="flex gap-6 flex-col md:flex-row-reverse md:mr-auto md:items-center">
+                          <div className="flex flex-col gap-1">
+                            <h4 className="text-base text-primary font-500">
+                              Available in packs of
+                            </h4>
+                            <div className="flex gap-2 flex-wrap">
+                              {product.available_packs
+                                ?.split(",")
+                                .map((pack, i) => (
+                                  <div
+                                    key={i}
+                                    className="text-base text-bodyText rounded bg-muted font-600 px-4 py-2"
+                                  >
+                                    {pack}
+                                  </div>
+                                ))}
+                            </div>
                           </div>
+                          <Button
+                            onClick={() => setIsUserSignUpModalOpen(true)}
+                            className="w-full md:w-[200px] py-2 md:py-3 min-w-fit"
+                            variant="secondary"
+                          >
+                            Inquire Now
+                          </Button>
                         </div>
-                        <Button
-                          onClick={() => setIsUserSignUpModalOpen(true)}
-                          className="w-full md:w-[200px] py-2 md:py-3 min-w-fit"
-                          variant="secondary"
-                        >
-                          Inquire Now
-                        </Button>
                       </div>
                     </div>
+                  ))
+                ) : (
+                  <div className="text-center h-40 flex items-center justify-center text-lg font-500 md:text-xl">
+                    No products found!
                   </div>
-                ))
+                )
               ) : (
                 <div className="text-center h-40 flex items-center justify-center text-lg font-500 md:text-xl">
-                  No products found
+                  Something went wrong!
                 </div>
               )}
             </>
